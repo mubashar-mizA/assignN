36b6c91b93903f3d920b36fe71646193964553c3
mubashar-mizA <mirzamubashar535@gmail.com>
2025-03-25 10:47:53 +0500

    Update Header.jsx

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -45,10 +45,25 @@ const Header = () => {
       {/* Mobile Navigation */}
       <div className={`absolute top-16 left-0 w-full bg-blue-600 text-white p-4 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
         <ul className="space-y-4">
-          <li><a href="#" className="block hover:underline">Home</a></li>
-          <li><a href="#" className="block hover:underline">About</a></li>
-          <li><a href="#" className="block hover:underline">Services</a></li>
-          <li><a href="#" className="block hover:underline">Contact</a></li>
+          <li>
+            <Link
+              href="/"
+              className="hover:underline">
+              Home</Link>
+          </li>
+
+          <li>
+            <Link
+              href="/pages/about"
+              className="hover:underline">
+              About</Link>
+          </li>
+
+          <li><Link
+            href="/pages/contact"
+            className="hover:underline">
+            Contact</Link>
+          </li>
         </ul>
       </div>
     </header>
