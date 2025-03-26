b665870cb83e818d9ac232c8bdf32bca0efd5b75
mubashar-mizA <mirzamubashar535@gmail.com>
2025-03-26 19:49:10 +0500

    removing unncessary comments in auth

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
     try {
-        const data = await req.json(); // "response" ki jagah direct "data"
+        const data = await req.json(); 
 
         return NextResponse.json({
             ...data,
