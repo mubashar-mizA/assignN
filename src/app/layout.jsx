61faf03ef41e9a0d042cc840d57b570f4f541e39
mubashar-mizA <mirzamubashar535@gmail.com>
2025-03-27 16:52:16 +0500

    modifying the main layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`antialiased max-w-6xl mx-auto`}
+        className={`antialiased `}
       >
         <Header />
         {children}
